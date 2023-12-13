097d2ed541c999360d84baff6c3a225548f8189a
birdslisten <justin.schultz37@gmail.com>
2023-12-13 14:33:55 -0800

    added file structure to start API routes, and created models for user, thought, and reaction

---
+++ models/Thought.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+const { Schema, model } = mongoose;
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (createdAtVal) => new Date(createdAtVal).toLocaleString(),
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        reactions: [ReactionSchema],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true,
+        },
+        id: false,
+    }
+);
+
+// Virtual to get the reaction count
+thoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+});
+
+const Thought = model('Thought', thoughtSchema);
+
+module.exports = Thought;
